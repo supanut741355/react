889079cce95b69c364a5d013a14bd090e7803267
supanut741355 <supanut.wizard@gmail.com>
2026-05-10 14:38:28 +0700

    add show animal

--- animals/src/App.js
+++ animals/src/App.js
@@ -2,23 +2,28 @@ import { useState } from "react";
 import { AnimalShow } from "./components/AnimalShow";
 
 
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'ant', 'bat', 'horse']
+  return animals[Math.floor(Math.random() * animals.length)]
+}
+
 function App() {
 
-  const [animalCounts, setAnimalCounts] = useState(0)
-
-
+  const [animals, setAnimal] = useState([])
   const handleAddAnimal = () => {
-    setAnimalCounts(animalCounts + 1)
+    setAnimal([...animals, getRandomAnimal()])
+    
   }
 
+  const renderAnimal = animals.map( (animal, idx) => {
+    return <AnimalShow type={animal} key={idx} />
+  })
   return (
     <div>
       <button onClick={handleAddAnimal}>Add animal</button>
-      <div>
-        Animal count {animalCounts}
-      </div>
+      {renderAnimal}
     </div>
-  );
+  )  
 }
 
 export default App;
--- animals/src/components/AnimalShow.js
+++ animals/src/components/AnimalShow.js
@@ -1,8 +1,29 @@
+import bird from '../svg/bird.svg'
+import cat from '../svg/cat.svg'
+import cow from '../svg/cow.svg'
+import dog from '../svg/dog.svg'
+import gator from '../svg/gator.svg'
+import heart from '../svg/heart.svg'
+import horse from '../svg/horse.svg'
 
-export const AnimalShow = () => {
+
+
+export const AnimalShow = (props) => {
+
+  const svgMap = {
+    bird,
+    dog,
+    cat,
+    cow,
+    gator,
+    heart,
+    horse
+  }
+
+  const {type} = props
   return (
     <div>
-      Cow!
+      <img alt="animal" src={svgMap[type]}/>
     </div>
   )
 }
