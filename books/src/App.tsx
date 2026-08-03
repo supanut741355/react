0dc6e46ca54c2308d51f4804d5a88e2a63c649ce
supanut741355 <supanut.wizard@gmail.com>
2026-08-03 23:56:44 +0700

    add bookCreate logic

--- books/src/App.tsx
+++ books/src/App.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react"
+import BookCreate from "./components/BookCreate"
+
 function App() {
+
+  const [books, setBooks] = useState<string[]>([])
+
+  const handleCreateBook = (newBook) => {
+    setBooks(books => [...books, newBook])
+    
+    console.log(books);
+  }
 
   return (
     <>
-      yo
+      <BookCreate onCreateBook={handleCreateBook}/>
+      {books.map( (item) => {
+        return <p>{item}</p>
+      })}
     </>
   )
 }
--- books/src/components/BookCreate.tsx
+++ books/src/components/BookCreate.tsx
@@ -1,7 +1,25 @@
-const BookCreate = () => {
+import { useState } from "react"
+
+const BookCreate = ({onCreateBook}) => {
+
+  const [book, setBook] = useState('')
+
+  const handleOnChange = (event) => {
+    setBook(event.target.value)
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+    onCreateBook(book)
+  }
+
   return (
     <div>
       BookCreate
+      <form onSubmit={handleFormSubmit}>
+        <input value={book} onChange={handleOnChange}/>
+        <button>Add</button>
+      </form>
     </div>
   )
 }
